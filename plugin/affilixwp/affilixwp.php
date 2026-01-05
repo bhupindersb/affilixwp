c7f6f05ce62c6f581d54b4469f9c3309250146fa
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-01-05 12:20:43 +0530

    Bump plugin version to 0.1.3

--- plugin/affilixwp/affilixwp.php
+++ plugin/affilixwp/affilixwp.php
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) exit;
 
 define('AFFILIXWP_PATH', plugin_dir_path(__FILE__));
 define('AFFILIXWP_URL', plugin_dir_url(__FILE__));
-define('AFFILIXWP_VERSION', '0.1.2');
+define('AFFILIXWP_VERSION', '0.1.3');
 
 require_once AFFILIXWP_PATH . 'includes/class-activator.php';
 require_once AFFILIXWP_PATH . 'includes/class-referrals.php';
