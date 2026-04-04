1facdf466eeb9576d318a5dfe6079d67682a69ee
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-04-04 16:46:27 +0530

    Update Version

--- affilixwp.php
+++ affilixwp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AffilixWP
  * Description: Affiliate & multi-level commission tracking for WordPress.
- * Version: 0.3.69
+ * Version: 0.3.70
  * Author: AffilixWP
  */
 
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) exit;
 
 define('AFFILIXWP_PATH', plugin_dir_path(__FILE__));
 define('AFFILIXWP_URL', plugin_dir_url(__FILE__));
-define('AFFILIXWP_VERSION', '0.3.69');
+define('AFFILIXWP_VERSION', '0.3.70');
 define('AFFILIXWP_UPDATE_URL', 'https://affilixwp.beveez.tech/update.json');
 define('FS_METHOD', 'direct');
 
