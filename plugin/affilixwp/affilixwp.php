66575a72121be7866fa4779e90a545e78aa7656e
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-04-04 13:04:44 +0530

    Secure Auto Deployment

--- plugin/affilixwp/affilixwp.php
+++ plugin/affilixwp/affilixwp.php
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) exit;
 
 define('AFFILIXWP_PATH', plugin_dir_path(__FILE__));
 define('AFFILIXWP_URL', plugin_dir_url(__FILE__));
-define('AFFILIXWP_VERSION', '0.3.61');
+define('AFFILIXWP_VERSION', '0.3.62');
 define('AFFILIXWP_UPDATE_URL', 'https://affilixwp.beveez.tech/update.json');
 
 /**
